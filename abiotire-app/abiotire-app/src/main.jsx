35ef8e1409ddec63ab62164da6ccef2937275f3b
anthony1573 <venanthony1573@gmail.com>
2025-06-04 12:38:27 +0800

    Add files via upload

---
+++ abiotire-app/abiotire-app/src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import AbiaoTireApp from './ui/AbiaoTireApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AbiaoTireApp />
+  </React.StrictMode>
+);
